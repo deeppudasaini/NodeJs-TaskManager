97908a60368485cfb1324fb2b2a8ae346f80eead
Deep Pudasaini <deep.pudasaini19@my.northampton.ac.uk>
2021-09-28 08:36:24 +0545

    All apis added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,22 @@
 const getAllTasks=(req,res)=>{
     res.send('get all tasks')
 }
+const createTasks=(req,res)=>{
+    res.send('create tasks');
+}
+const getSingleTask=(req,res)=>{
+    res.send('get Single task');
+}
+const updateTask=(req,res)=>{
+    res.send('update tasks');
+}
+const deleteTasks=(req,res)=>{
+    res.send('delete tasks');
+}
 module.exports={
-    getAllTasks
+    getAllTasks,
+    createTasks,
+    getSingleTask,
+    updateTask,
+    deleteTasks
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,19 +1,9 @@
 const express=require('express');
-const {getAllTasks}=require('../controllers/tasks');
+const {getAllTasks,createTasks,getSingleTask,updateTask,deleteTasks}=require('../controllers/tasks');
 const router=express.Router();
 
-router.route('/').get(getAllTasks);
-router.route('/').post((req,res)=>{
-    res.send('add Tasks');
-    });
-router.route('/:taskId').get((req,res)=>{
-    res.send(`Single Tasks number `+req.params.taskId);
-        });
-router.route('/:taskId').put((req,res)=>{
-    res.send(`Update Tasks number ${req.params.taskId}`);
-        });
-router.route('/:taskId').delete((req,res)=>{
-    res.send(`Delete Tasks number ${taskId}`);
-        }
-    );
+router.route('/').get(getAllTasks).post(createTasks);
+router.route('/:taskId').get(getSingleTask);
+router.route('/:taskId').put(updateTask);
+router.route('/:taskId').delete(deleteTasks);
 module.exports=router;
